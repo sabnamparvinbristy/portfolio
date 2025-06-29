b3a8cdc1f37633cce66d002d72459364919984a0
sabnamparvinbristy <parvin.sabnam.002@gmail.com>
2025-06-29 16:19:15 +0600

    import all component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
-
-import './App.css'
+import React from "react"
+import Navbar from "./components/Navbar/Navbar"
+import About from "./components/About/About"
+import Skill from "./components/Skills/Skill"
+import Work from "./components/Work/Work"
+import Education from "./components/Education/Education"
+import Contact from "./components/Contact/Contact"
+import Footer from "./components/Footer/Footer"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
