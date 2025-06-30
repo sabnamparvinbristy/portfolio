61a276d6709ba93447938db88f61aa68c5d74fe3
sabnamparvinbristy <parvin.sabnam.002@gmail.com>
2025-06-30 22:25:45 +0600

    blurBlob add

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // ✅ Make sure it's pointing to your files
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes:{
+        blob: {
+          '0%' : {transform : 'scale(1)'},
+          '33%' : {transform : 'scale(1.2)'},
+          '66%' : {transform : 'scale(0.8)'},
+          '100%' : {transform : 'scale(1)'},
+        },
+      },
+      animation:{
+        blob: 'blob 10s infinite',
+      }
+    },
   },
   plugins: [],
 }
